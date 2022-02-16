a00ceca8320259f3f2457fa6baf402bc9cb15aaa
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2022-02-16 11:31:21 -0700

    Added a player class to Main.py

--- Main.py
+++ Main.py
@@ -1,40 +1,23 @@
 import pyxel
-print("Hello World!")
+
+player = None
+
 class App():
     def __init__(self):
         # Initiate Window
         pyxel.init(128, 88, "Ya Ya's Game")
 
-        pyxel.load("CastleSet.pyxres", True, True, False, False)
-        
-        # Player variables
-        self.playerx = 0
-        self.playery = 35
-        self.playerColor = 2
+        pyxel.load("greenknight.pyxres", True, True, False, False)
 
+        global player
+        player = Player(0, 0)
+        
         # Run Application
         pyxel.run(self.Update, self.Draw)
 
     def Update(self):
         # Calls everythin in update player every frame
-        self.UpdatePlayer()
-
-    def UpdatePlayer(self):
-
-        # Player controls
-        if pyxel.btn(pyxel.KEY_LEFT) :
-            self.playerx -= 1
-        if pyxel.btn(pyxel.KEY_RIGHT) :
-            self.playerx +=1
-        if pyxel.btn(pyxel.KEY_DOWN) :
-            self.playery += 1
-        if pyxel.btn(pyxel.KEY_UP) :
-            self.playery -=1
-
-        if pyxel.btn(pyxel.KEY_W) :
-            self.playerColor += 1
-            if self.playerColor == 16 :
-                self.playerColor = 1
+        player.Update()
 
     def DrawMap(self) :
         # Drawing the tileset
@@ -42,18 +25,42 @@ class App():
         pyxel.bltm(0, 0, 0, 0, 0, 1200, 94)
 
         # Drawing text
-        #pyxel.text(2, 66, "Player Y: " + str(self.playery), 7)
-        #pyxel.text(2, 73, "Player X: " + str(self.playerx), 7)
-        
-    # Draws the player 
-    def DrawPlayer(self):
-        pyxel.blt(self.playerx, self.playery, 0, 0, 16, 8, 16, 0 )
-
+        pyxel.text(2, 66, "Player Y: " + str(player.y), 7)
+        pyxel.text(2, 73, "Player X: " + str(player.x), 7)
 
     # Draws all objects listed every frame
     def Draw(self) :
         self.DrawMap()
+        
         # Draws player every frame.
-        self.DrawPlayer()
+        player.Draw()
+        
+class Player() :
+    def __init__(self, x, y) :
+        # Player variables
+        self.x = x
+        self.y = x
+        
+        '''
+        pyxel.blt(self.x, self.y, 1, 8, 0, 8, 16, 0 )
+        pyxel.blt(self.x, self.y, 1, 8, 0, 8, 16, 0 )
+        pyxel.blt(self.x, self.y, 1, 8, 0, 8, 16, 0 )
+        '''
+        
+    # Draws the player 
+    def Draw(self):
+        pyxel.blt(self.x, self.y, 1, 8, 0, 8, 16, 0 )
+
+    def Update(self):
+        # Player controls
+        if pyxel.btn(pyxel.KEY_LEFT) :
+            self.x = 1
+        if pyxel.btn(pyxel.KEY_RIGHT) :
+            self.x +=1
+        if pyxel.btn(pyxel.KEY_DOWN) :
+            self.y += 1
+        if pyxel.btn(pyxel.KEY_UP) :
+            self.y -=1
+
 
 App()
