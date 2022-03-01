2dc2088df8e1eb600c2391015f879ad11bcf849a
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2022-02-28 18:57:17 -0700

    Added left and right colliders.

--- Main.py
+++ Main.py
@@ -5,27 +5,25 @@ FLOOR_TILE = (5, 0)
 TORCH_TILE = (3, 0)
 FLOOR_TILE_BOX = (5, 1)
 
-TORCH_TILES = [(1, 0), (2, 0), (3, 0)]
+TORCH_TILES = [(1, 0), (2, 0), (3, 0), (4, 0)]
 
-COLLIDE_X = 4
-
-collided = False
+COLLIDE_X = 5
 
 
 def getTile(x, y):
     return pyxel.tilemap(0).pget(x, y)
 
 
-def detectTile():
+def updateMap():
     for currentX in range(0, 16):
         for currentY in range(0, 10):
             currentTile = getTile(currentX, currentY)
 
-            if currentTile[0] >= COLLIDE_X:
-                pyxel.tilemap(0).pset(currentX, currentY, FLOOR_TILE_BOX)
+            # if currentTile[0] >= COLLIDE_X:
+            #     pyxel.tilemap(0).pset(currentX, currentY, FLOOR_TILE_BOX)
 
             if currentTile in TORCH_TILES:
-                u = ((pyxel.frame_count // 7 % 3) + 1) * 1
+                u = ((pyxel.frame_count // 6 % 4) + 1) * 1
                 drawTile = (u, 0)
                 pyxel.tilemap(0).pset(currentX, currentY, drawTile)
 
@@ -48,13 +46,12 @@ class App:
     @staticmethod
     def Update():
         player.UpdateControls()
-        detectTile()
 
     @staticmethod
     def DrawMap():
         # Drawing the tile-set
         pyxel.bltm(0, 0, 0, 0, 0, 1200, 94)
-        detectTile()
+        updateMap()
 
     # Draws all objects listed every frame
     def Draw(self):
--- player.py
+++ player.py
@@ -1,5 +1,26 @@
 import pyxel
 
+COLLIDE_X = 4
+
+
+def getTile(x, y):
+    return pyxel.tilemap(0).pget(x, y)
+
+
+def detectTile(x, y, dy):
+    x1 = int(x // 8)
+    y1 = int(y // 8)
+    x2 = int((x + 8) // 8)
+    y2 = int((y + 8) // 8)
+    for yi in range(y1, y2 + 1):
+        for xi in range(x1, x2 + 1):
+            if getTile(xi, yi)[0] >= COLLIDE_X:
+                return True
+            # pyxel.tilemap(0).pset(xi, yi, (0, 0))
+
+    return False
+
+
 class Player:
     def __init__(self, x, y):
         # Player variables
@@ -10,13 +31,10 @@ class Player:
         self.dy = 0
         self.speed = 0.8
 
-        self.playerWidth = 8
-
         # Keyboard Variables
         self.leftDown = False
         self.rightDown = False
         self.haveJumped = False
-
         self.direction = 1
 
         # Buttons
@@ -27,28 +45,24 @@ class Player:
         pyxel.load("greenknight.pyxres")
 
     def Draw(self):
-        u = (pyxel.frame_count // 8 % 3 + 1) * 8 if self.leftDown or self.rightDown else 8
+        u = (pyxel.frame_count // 6 % 3 + 1) * 8 if self.leftDown or self.rightDown else 8
         v = 8 if self.direction == 1 else -8
         pyxel.blt(self.x, self.y, 1, u, 0, v, 16, 0)
 
     def Move(self):
         if self.leftDown:
             self.direction = -1
-            if self.dx > 0.0:
-                self.dx = self.speed
-                self.dx = -self.dx
-            if self.x > 0:
-                self.x += self.dx
+            if not detectTile(self.x, self.y, self.dy):
+                self.x -= self.dx
+            else:
+                pass
 
         if self.rightDown:
             self.direction = 1
-            if self.dx < 0.0:
-                self.dx = self.speed
-            if self.x + self.playerWidth < 128:
+            if not detectTile(self.x + 1, self.y, self.dy):
                 self.x += self.dx
-
-        if self.dy > 0.0:
-            pass
+            else:
+                pass
 
     def UpdateControls(self):
         if pyxel.btnp(self.left):
