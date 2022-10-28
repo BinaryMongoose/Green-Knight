cef5cef562234336277aa53fb93c6ada828426ea
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2022-10-27 19:00:32 -0700

    Started new development of Greenknight.

--- OldScripts/Main2.py
+++ OldScripts/Main2.py
@@ -1,4 +1,4 @@
-import pyxel
+import pyxel as pyx
 from player import Player
 
 FLOOR_TILE = (5, 0)
@@ -6,60 +6,45 @@ TORCH_TILE = (3, 0)
 FLOOR_TILE_BOX = (5, 1)
 
 TORCH_TILES = [(1, 0), (2, 0), (3, 0), (4, 0)]
-
 COLLIDE_X = 5
+SCREEN_WIDTH = 128
+SCREEN_HEIGHT = 88
 
 
 def getTile(x, y):
     """ Gets a tile from the displayed tile-map and returns the tile's x and y from the image bank """
-    return pyxel.tilemap(0).pget(x, y)
+    return pyx.tilemap(0).pget(x, y)
 
 
 def updateMap():
     """ Finds specific tiles off the map and animates the tiles """
-    for currentX in range(0, 16):
-        for currentY in range(0, 10):
+    for currentX in range(0, int(SCREEN_WIDTH / 8)):
+        for currentY in range(0, int(SCREEN_HEIGHT / 8)):
             currentTile = getTile(currentX, currentY)
 
             if currentTile in TORCH_TILES:
-                u = ((pyxel.frame_count // 6 % 4) + 1) * 1
+                u = ((pyx.frame_count // 6 % 4) + 1)
                 drawTile = (u, 0)
-                pyxel.tilemap(0).pset(currentX, currentY, drawTile)
+                pyx.tilemap(0).pset(currentX, currentY, drawTile)
 
 
-# noinspection PyGlobalUndefined
 class App:
-    """ THe main game """
+    """ The main game """
+
     def __init__(self):
         """ Contains all code for the startup of the game """
-        # noinspection PyArgumentList
-        pyxel.init(128, 88, "Green Knight")
-
-        pyxel.load("../Assets/greenknight.pyxres")
+        pyx.init(SCREEN_WIDTH, SCREEN_HEIGHT, "Green Knight")
+        pyx.load("../Assets/greenknight.pyxres")
 
         global player
-        player = Player(10, 34)
+        player = Player(10, 32)
 
         # Run Application
-        pyxel.run(self.Update, self.Draw)
+        pyx.run(self.Update, self.Draw)
 
-    @staticmethod
     def Update():
         """ Updates everything listed every frame, including input """
         player.UpdateControls()
 
-    @staticmethod
     def DrawMap():
         """ Draws the tile-map """
-        pyxel.bltm(0, 0, 0, 0, 0, 1200, 94)
-        updateMap()
-
-    def Draw(self):
-        """ Draws everything listed every frame """
-        self.DrawMap()
-        player.Draw()
-
-        # pyxel.text(5, 70, , 7)
-
-
-App()
--- OldScripts/player.py
+++ OldScripts/player.py
@@ -1,11 +1,12 @@
-import pyxel
+import pyxel as pyx
 
 COLLIDE_X = 4
+TILE_FLOOR = (5, 0)
 
 
 def getTile(x, y):
     """ Gets a tile from the displayed tile-map and returns the tile's x and y from the image bank """
-    return pyxel.tilemap(0).pget(x, y)
+    return pyx.tilemap(0).pget(x, y)
 
 
 def detectTile(x, y, dy):
@@ -19,19 +20,25 @@ def detectTile(x, y, dy):
             if getTile(xi, yi)[0] >= COLLIDE_X:
                 return True
 
+    if dy > 0 and y % 8 == 1:
+        for xi in range(x1, x2 + 1):
+            if getTile(xi, y1 + 1) == TILE_FLOOR:
+                return True
     return False
 
 
 class Player:
     """ Creates a player object that accepts input, collides and is animated """
+
     def __init__(self, x, y):
         # Player variables
         self.x = x
         self.y = y
 
-        self.dx = 1.0
+        self.dx = 1
         self.dy = 0
         self.speed = 0.8
+        self.jumpHeight = 5
 
         # Keyboard Variables
         self.leftDown = False
@@ -40,46 +47,61 @@ class Player:
         self.direction = 1
 
         # Buttons
-        self.right = pyxel.KEY_D
-        self.left = pyxel.KEY_A
-        self.jump = pyxel.KEY_SPACE
+        self.right = pyx.KEY_D
+        self.left = pyx.KEY_A
+        self.jump = pyx.KEY_SPACE
 
-        pyxel.load("../Assets/greenknight.pyxres")
+        pyx.load("../Assets/greenknight.pyxres")
 
     def Draw(self):
         """ Animates the player """
-        u = (pyxel.frame_count // 6 % 3 + 1) * 8 if self.leftDown or self.rightDown else 8
+
+        # u is the variable that holds the actual image from the image bank.
+        # It loops through the images using calculations
+        u = (pyx.frame_count // 6 % 3 + 1) * 8 if self.leftDown or self.rightDown else 8
+
+        # v decides if the image needs to be flipped. Positive for no, negative for yes.
         v = 8 if self.direction == 1 else -8
-        pyxel.blt(self.x, self.y, 1, u, 0, v, 16, 0)
+        pyx.blt(self.x, self.y, 1, u, 0, v, 16, 0)
 
     def Move(self):
         """ Alters player x and y based on direction x and y and player input """
+
+        # Checks if leftDown is true, then move the player
         if self.leftDown:
             self.direction = -1
             if not detectTile(self.x, self.y, self.dy):
                 self.x -= self.dx
-            else:
-                pass
 
+        # Checks if rightDown is true, then move the player
         if self.rightDown:
             self.direction = 1
             if not detectTile(self.x + 1, self.y, self.dy):
                 self.x += self.dx
-            else:
-                pass
 
     def UpdateControls(self):
         """ Checks if the player pressed any keys and call Move function """
-        if pyxel.btnp(self.left):
+        pyx.tilemap(0).pset(3, 0, TILE_FLOOR)
+
+        # Checking button presses
+        if pyx.btnp(self.left):
             self.leftDown = True
-        if pyxel.btnp(self.right):
+
+        if pyx.btnp(self.right):
             self.rightDown = True
 
-        if pyxel.btnr(self.left):
+        # Checking button releases
+        if pyx.btnr(self.left):
             self.leftDown = False
             self.dx = self.speed
-        if pyxel.btnr(self.right):
+
+        if pyx.btnr(self.right):
             self.rightDown = False
             self.dx = self.speed
+
+        if pyx.btnr(self.jump):
+            if not self.haveJumped:
+                self.haveJumped = True
+                self.dy = self.jumpHeight
 
         self.Move()
